ef2d7003557367928370fcf9e78fd7dc8761505b
Brittany Hancock <femaleentp@gmail.com>
2024-05-21 11:52:30 -0700

    created and refined Edge and Vertex classes.  Developed two test methods.

---
+++ GraphLib/Edge.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphLib
+{
+    public class Edge
+    {
+        public Vertex Parent { get; set; }
+
+        public Vertex Child { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
--- GraphTests/DirectedGraphTests.cs
+++ GraphTests/DirectedGraphTests.cs
@@ -1,11 +1,46 @@
+using GraphLib;
 namespace GraphTests
 {
     [TestClass]
     public class DirectedGraphTests
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //}
+        [TestMethod]
+        public void AddingANodeToTheGraph_ShouldReturnAVertex()
+        {
+            // arrange
+            DirectedGraph graph = new DirectedGraph();
+
+            // act
+            var result = graph.AddVertex("Toast");
+
+            // assert
+            Assert.IsInstanceOfType<Vertex>(result);
+            Assert.AreEqual(result.Label, "Toast");
+        }
+
+        [TestMethod]
+        [DataRow("Toast", "Butter", "Lawnmower")]
+        public void AddingMoreThanOneNodeToTheGraph_ShouldRetainEachOfThem(string one, string two, string three)
+        {
+            // arrange
+            DirectedGraph graph = new DirectedGraph();
+
+            // act
+            graph.AddVertex(one);
+            graph.AddVertex(two);
+            graph.AddVertex(three);
+
+            var result = graph.NumVertices;
+
+            var resultOne = graph.Vertices[0];
+            var resultTwo = graph.Vertices[1];
+            var resultThree = graph.Vertices[2];
+
+            // assert
+            Assert.AreEqual(3, result);
+            Assert.AreEqual(one, resultOne.Label);
+            Assert.AreEqual(two, resultTwo.Label);
+            Assert.AreEqual(three, resultThree.Label);
+        }
     }
 }
